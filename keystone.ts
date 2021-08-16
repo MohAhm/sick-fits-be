366a75b8ed3dcb222ca6baf1ee8672edd433352b
Mohamed Ali <a.mohamed.cali@gmail.com>
2021-08-16 23:31:42 +0200

    Add first User data

--- keystone.ts
+++ keystone.ts
@@ -1,4 +1,10 @@
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { createAuth } from '@keystone-next/auth';
+import { User } from './schemas/User';
 import 'dotenv/config';
 
 const databaseURL =
@@ -9,24 +15,43 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
   },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-    // TODO: Add data seeding here
-  },
-  lists: createSchema({
-    // Schema items go in here
-  }),
-  ui: {
-    // TODO: change this for notes
-    isAccessAllowed: () => true,
-  },
-  // TODO: Add session values here
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: Add data seeding here
+    },
+    lists: createSchema({
+      // Schema items go in here
+      User,
+    }),
+    ui: {
+      // TODO: change this for notes
+      isAccessAllowed: ({ session }) => {
+        console.log(session);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        return !!session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQL Query
+      User: 'id',
+    }),
+  })
+);
